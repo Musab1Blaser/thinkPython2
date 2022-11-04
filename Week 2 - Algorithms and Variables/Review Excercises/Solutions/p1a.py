0dce6e7eef094be132f4d2c3df2c1f92b3f24316
Musab1Blaser <musab16000@gmail.com>
2022-11-04 14:53:22 +0500

    week 2 - p1 solutions

---
+++ Week 2 - Algorithms and Variables/Review Excercises/Solutions/p1a.py
@@ -0,0 +1,13 @@
+dist_in_km = 10
+time_in_minutes = 42
+time_in_seconds = 42
+
+km_per_mile = 1.61
+miles = dist_in_km / km_per_mile
+
+total_time_in_seconds = 60 * time_in_minutes + time_in_seconds
+time_in_hours = total_time_in_seconds/3600
+
+speed = miles/time_in_hours
+
+print(speed)
